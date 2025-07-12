2ef3f5a8a11c1a4e1bbeec3a5ebaae0c4819d31e
Hammad Ali Khan <hammad.web3.0developer@gmail.com>
2025-07-12 16:28:42 +0500

    add next-auth-js

---
+++ src/app/api/auth/register/page.ts
@@ -0,0 +1,50 @@
+import User from "@/model/Users";
+import { NextRequest, NextResponse } from "next/server";
+import { ConnectToDb } from "@/lib/db";
+
+
+
+
+
+async function POST(request:NextRequest){
+
+    
+    try {
+
+    const {email,password} = await request.json()
+
+
+    if(!email || !password){
+        return NextResponse.json({error:"Invalid credentials"},{status:400})
+    }
+        await ConnectToDb()
+        
+        const existingUser = await User.findOne({email:email})
+    
+        if(existingUser){
+            return NextResponse.json({error:"User already existed"},{status:400})
+        }
+         
+        const user = await User.create({email,password})
+    
+        return NextResponse.json(
+            {
+                message:"User created successfully",
+                user
+            },{
+                status:200
+            }
+        )
+    
+    } catch (error) {
+        console.log("Registration Error",error);
+        return NextResponse.json({error:"Registration failed"},{status:500})
+                       
+    }
+
+
+
+
+
+
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,82 @@
+import User from "@/model/Users";
+import { NextAuthOptions} from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import { ConnectToDb } from "./db";
+import bcrypt from "bcryptjs";
+
+
+
+export const authOptions:NextAuthOptions = {
+      providers:
+      [
+      CredentialsProvider({
+       
+        name: 'Credentials',
+        
+        credentials: {
+          email: { label: "Email", type: "email", },
+          password: { label: "Password", type: "password" }
+        },
+
+        async authorize (credentials) {
+            if(!credentials?.email || !credentials?.password){
+                throw new Error("Invalid credentials")
+            }
+
+               try {
+                 await ConnectToDb()
+                 const user =await User.findOne({email:credentials.email})
+ 
+                 if(!user){
+                     throw new Error("No user found")
+                 }
+
+                 const isValid = await bcrypt.compare(credentials?.password,user.password)
+
+                 if(!isValid){
+                     throw new Error("Invalid credentials")
+                 }
+
+                 return {
+                    id:user._id.toString(),
+                    email:user.email,
+                 }
+
+               } catch (error) {
+                console.log("Auth Error",error);
+                throw error                  
+               }
+
+            }
+        
+        
+      }),
+
+    
+    ],
+
+    callbacks:{
+        async jwt({ token, user }) {
+            if(user){
+                token.id = user.id
+            }
+            return token
+          },
+          
+          async session({ session, token }) {
+            if(session.user){
+                session.user.id = token.id as string
+            }
+              return session
+            },
+    },
+    pages:{
+        signIn: '/login',
+        error: '/login', 
+    },
+    session:{
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60
+    },
+    secret:process.env.NEXTAUTH_SECRET,
+}
